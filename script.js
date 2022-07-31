50490832d7961af47753d8e2ea21d47c6216943c
Clermondo <Isaac.ErismeC@gmail.com>
2022-07-31 02:31:28 -0400

    Api information being display on webpage

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let weather = {
             "https://api.openweathermap.org/data/2.5/weather?q="
             + city +"&appid=" 
             + this.apiKey
+            + "&units=imperial"
         )
         .then((response) => response.json())
         .then((data) => this.displayWeather(data));
@@ -15,6 +16,13 @@ let weather = {
         const {temp, humidity} = data.main
         const {speed} = data.wind
         console.log(name, icon, description, temp, humidity, speed)
-        
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+        document.querySelector(".description").innertext = description;
+        document.querySelector(".temp").innerText = temp + "°" 
+        document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%"
+        document.querySelector(".wind").innerText = "Wind speed: " + "km/h"
     }
 };
+
+document.querySelector(".search button")
